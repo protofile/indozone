242446256e67a63e3e85600894dad973d02732fe
yudantoardi <yudantoardi@gmail.com>
2025-04-30 15:10:01 +0700

    updates

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'z-red': {
+          600: '#E6465C'
+        }
+      },
+      fontFamily:{
+        'exo': '"Exo", sans-serif'
+      },
+      fontSize: {
+        'xsm': ['.8125rem', {
+          lineHeight: '1rem',
+        }],
+        'xxs': ['.6875rem', {
+          lineHeight: '1rem',
+        }],
+      },
+      lineHeight: {
+        'leading-half': '0.5rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
